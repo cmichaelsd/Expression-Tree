551cfbc2db92c8f9066cbf0e18f0688b810aa873
Cole Michaels <cmichaelsd@gmail.com>
2020-07-30 10:25:31 +0900

    added private properties for stack.

--- stack.py
+++ stack.py
@@ -2,8 +2,20 @@ from typing import Any
 
 class Stack:
     def __init__(self):
-        self.top = -1
-        self.array = []
+        self.__top = -1
+        self.__array = []
+
+    @property
+    def top(self) -> int:
+        return self.__top
+
+    @top.setter
+    def top(self, value):
+        self.__top = value
+    
+    @top.deleter
+    def top(self, value):
+        self.__top = value
     
     def is_empty(self) -> bool:
         if self.top == -1:
@@ -11,14 +23,14 @@ class Stack:
         return False
     
     def peek(self) -> Any:
-        return self.array[-1]
+        return self.__array[-1]
 
     def pop(self) -> Any:
         if not self.is_empty():
             self.top -= 1
-            return self.array.pop()
+            return self.__array.pop()
         return "$"
 
     def push(self, value) -> None:
         self.top += 1
-        self.array.append(value)
+        self.__array.append(value)
